f6c15441e78c68d845deb9c9c6c57cdc83ca0891
prajapatip00ja <poojap@thoughtworks.com>
2015-05-13 12:43:55 +0530

    activity lifecycle is done

--- app/src/main/java/com/example/poojap/watch_app/WatchListingActivity.java
+++ app/src/main/java/com/example/poojap/watch_app/WatchListingActivity.java
@@ -1,16 +1,20 @@
 package com.example.poojap.watch_app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.poojap.watch_app.adapter.WatchListAdapter;
 
 
-public class WatchListingActivity extends ActionBarActivity {
+public class WatchListingActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,52 @@ public class WatchListingActivity extends ActionBarActivity {
         WatchListAdapter watchListAdapter = new WatchListAdapter(this,new String[7]);
         ListView listView = (ListView) findViewById(R.id.list_of_watch);
         listView.setAdapter(watchListAdapter);
+        Log.d("activity","create");
+
     }
+
+    public void goToSecondPage(View view){
+        Intent intent = new Intent(this,secondActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("activity", "start");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("activity", "restart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onRestart();
+        Log.d("activity", "resume");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onRestart();
+        Log.d("activity", "stop");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("activity","pause");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("activity","destroy");
+    }
+
+
 
 
 
